0439424e47464b71477a540a77b8d4e1d300e4fd
zorlubhayat <isa.zorlu@stud.fh-campuswien.ac.at>
2025-01-21 15:48:17 +0100

    Delete Connect4-ProtocolTopG directory

--- Connect4-ProtocolTopG/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.connect4protocoltopg {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.connect4protocoltopg to javafx.fxml;
-    exports com.example.connect4protocoltopg;
-}
